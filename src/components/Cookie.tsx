7117a994d9099887abfec3b22a7a682aff0b56b4
Bruno Mourinha <bruno.mourinha@vortal.biz>
2021-03-02 18:16:36 +0000

    init cookie

---
+++ src/components/Cookie.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface Props {}
+
+const Cookie = (props: Props) => {
+  return <div>Cookie</div>;
+};
+
+export default Cookie;
---
+++ src/pages/CookieFactory.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Cookie from "../components/Cookie";
+
+interface Props {}
+
+const CookieFactory = (props: Props) => {
+  return (
+    <div>
+      <Cookie />
+    </div>
+  );
+};
+
+export default CookieFactory;
