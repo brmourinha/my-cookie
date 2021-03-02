5066c21de76805e2b3c4c16af398e8f3ac140625
Bruno Mourinha <bruno.mourinha@vortal.biz>
2021-03-02 18:46:20 +0000

    add cookie and add cursor

--- src/components/Cookie.tsx
+++ src/components/Cookie.tsx
@@ -1,9 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 interface Props {}
 
+interface ICursor {
+  own: number;
+  price: number;
+  production: number;
+}
+
 const Cookie = (props: Props) => {
-  return <div>Cookie</div>;
+  const [total, setTotal] = useState(0);
+  const [cursor, setCursor] = useState<ICursor>({
+    own: 0,
+    price: 15,
+    production: 10000,
+  });
+
+  const handleSum = () => {
+    setTotal((prevState) => prevState + 1);
+  };
+
+  useEffect(() => {
+    if (cursor.own > 0) {
+      setInterval(() => {
+        handleSum();
+      }, cursor.production);
+    }
+  }, [cursor]);
+
+  const addCursor = () => {
+    setTotal((prevState) => prevState - cursor.price);
+    setCursor({ ...cursor, own: cursor.own + 1, price: cursor.price + 1 });
+  };
+
+  return (
+    <>
+      <div>{total}</div>
+      <div>
+        <button onClick={() => setTotal(total + 1)}>Add Cookie</button>
+      </div>
+      <div>
+        <button disabled={total < cursor.price} onClick={addCursor}>
+          Add Cursor
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default Cookie;
